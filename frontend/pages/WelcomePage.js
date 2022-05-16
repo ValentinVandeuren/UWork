2d8494283a658d2300c8f989a78aae3a9d417919
Jimmy Cabuy <jimmycabuy@hotmail.com>
2022-05-16 12:25:53 +0200

    welcomepage

--- frontend/pages/WelcomePage.js
+++ frontend/pages/WelcomePage.js
@@ -1,13 +1,25 @@
-import { View, Button, StyleSheet } from 'react-native'
+import { View, StyleSheet, Image, Text, Button, TouchableOpacity } from 'react-native'
 import React from 'react'
+
 
 export default function WelcomePage(props) {
   return (
     <View style={styles.container}>
-      <Button
-      title='WelcomePage'
-      onPress={() => props.navigation.navigate('SignInPage')}
-      />
+      <Image source={require('../assets/logo.png')} style={styles.logo}/>
+        <View style={{alignItems:'center', marginTop:125}}>
+          <Text style={{fontSize: 30, textAlign:'center', paddingBottom: 30, color:"#000", fontWeight: '600' }}>Welcome on Uwork 👋🏼</Text>
+          <Text style={{fontSize: 25, textAlign:'center', paddingBottom: 30, color:"#000", fontWeight:'500' }}>Who are <Text style={{color:'#7791DE'}}>U</Text>?</Text>
+          <TouchableOpacity style={styles.button1} /*onPress={() => {props.navigation.navigate('SignInPage')}}*/>
+            <Text style={{color:"#fff", fontSize:20, fontWeight:'600'}}>Company</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button1} onPress={() => {props.navigation.navigate('SignUpPage')}}>
+            <Text style={{color:"#fff", fontSize:20, fontWeight:'500'}}>Employee</Text>
+          </TouchableOpacity>
+          <Text style={{marginBottom:10, fontSize:15, fontWeight:'500'}}>Or</Text>
+          <TouchableOpacity style={styles.button2} onPress={() => {props.navigation.navigate('SignInPage')}}>
+            <Text style={{color:"#7791DE", fontSize:20, fontWeight:'500'}}>Sign-in</Text>
+          </TouchableOpacity>
+      </View>
     </View>
   )
 }
@@ -15,11 +27,52 @@ export default function WelcomePage(props) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#F3F3F3',
+    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
   },
-  button: {
-    
-  }
+  logo: {
+    width:143,
+    height:100,
+    marginTop:100
+  },
+  button1: {
+    borderRadius:25,
+    paddingTop: 5,
+    paddingBottom: 5,
+    paddingLeft: 50,
+    paddingRight: 50,
+    backgroundColor: '#7791DE',
+    shadowColor: '#000',
+    shadowOpacity: 0.15,
+    elevation: 9,
+    shadowRadius: 3.84,
+    shadowOffset : { width: 0, height: 3 },
+    color:"#fff",
+    marginBottom:10,
+    height:50,
+    width: 200,
+    justifyContent:"center",
+    alignItems:'center',
+  },
+  button2: {
+    borderRadius:25,
+    paddingTop: 5,
+    paddingBottom: 5,
+    paddingLeft: 50,
+    paddingRight: 50,
+    backgroundColor: '#fff',
+    shadowColor: '#000',
+    shadowOpacity: 0.15,
+    elevation: 9,
+    shadowRadius: 3.84,
+    shadowOffset : { width: 0, height: 3 },
+    color:"#fff",
+    marginBottom:10,
+    height:50,
+    width: 200,
+    justifyContent:"center",
+    alignItems:'center',
+    borderWidth:3,
+    borderColor:"#7791DE"
+  },
 });
