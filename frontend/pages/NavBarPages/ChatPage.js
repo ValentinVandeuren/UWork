4e8ff7717b44b1fd322a31a2839c1463bd68571d
Valentin Vandeuren <valentinvandeuren.code@gmail.com>
2022-05-20 17:01:34 +0200

    chatpageback

--- frontend/pages/NavBarPages/ChatPage.js
+++ frontend/pages/NavBarPages/ChatPage.js
@@ -1,11 +1,34 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text, StyleSheet, TextInput, ScrollView, KeyboardAvoidingView, Image, TouchableOpacity} from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { connect } from 'react-redux';
 
-export default function ChatPage(props) {
+export function ChatPage(props) {
+  let [userId, setUserId] = useState("");
+  let [userisSet, setUserIsSet] = useState(false);
+
   let [messageInput, setMessageInput] = useState("");
   let [otherAvatar, setOtherAvatar] = useState();
   let [seeHourSend, setSeeHourSend] = useState(false);
+  let [message, setMessage] = useState([]);
+
+  useEffect(() => {  
+    ( () => {
+      AsyncStorage.getItem("id", function(error, data) {
+        setUserId(data);
+        setUserIsSet(true);
+      });
+    })();
+    let arrayConversation = [];
+    const loadConversation = async() => {
+      let responseConversation = await fetch(`http://172.20.10.2:3000/chat/getChat/${props.conversationId}`)
+      let response = await responseConversation.json()
+      arrayConversation.push(response.messages)
+      setMessage(arrayConversation)
+    }
+    loadConversation()
+  }, []);
 
   const onMessageClick = () => {
     if(seeHourSend){
@@ -15,70 +38,84 @@ export default function ChatPage(props) {
     }
   }
 
-  return (
-    <View style={styles.container}>
-      <View style={styles.topPage}>
-        <Ionicons
-          name={'chevron-back'}
-          size={40} color={'#7791DE'}
-          style={{marginLeft: 10, marginBottom: 5}}
-          onPress={() => props.navigation.navigate('ConversationPage')}
-        />
-        <View>
-          <Image
-            source={{uri: otherAvatar}}
-            style={styles.avatarConversation}
+  console.log(message);
+  console.log(message.length);
+
+  if(message.length >=0){
+    return (
+      <View style={styles.container}>
+        <View style={styles.topPage}>
+          <Ionicons
+            name={'chevron-back'}
+            size={40} color={'#7791DE'}
+            style={{marginLeft: 10, marginBottom: 5}}
+            onPress={() => props.navigation.navigate('ConversationPage')}
           />
-          <Text>Carrefour</Text>
+          <View>
+            <Image
+              source={{uri: otherAvatar}}
+              style={styles.avatarConversation}
+            />
+            <Text>Carrefour</Text>
+          </View>
+          <View style={{width: 40, marginRight: 10}}></View>
         </View>
-        <View style={{width: 40, marginRight: 10}}></View>
-      </View>
-      <ScrollView style={{flex: 1, width: "100%"}}>
-        <View style={styles.containerMessage}>
-          <TouchableOpacity onPress={() => onMessageClick()}>
-            <View style={styles.boxMessage}>
-              <Text style={styles.message}>Bonjour Jimmy,{"\n"}Merci pour votre intérêt pour DITB. Nous organisons les entretiens bientôt, nous vous tiendrons au courant. Belle journée!</Text>
+        <ScrollView style={{flex: 1, width: "100%"}}>
+          {message[0].map((messages,i) => (
+            <View key={i} style={(userId === messages.sender)?styles.containerMessageOwner: styles.containerMessage}>
+              <TouchableOpacity onPress={() => onMessageClick()}>
+                <View style={(userId === messages.sender)?styles.boxMessageOwner: styles.boxMessage}>
+                  <Text style={(userId === messages.sender)?styles.messageOwner: styles.message}>{messages.content}</Text>
+                </View>
+                <Text style={(seeHourSend)? styles.sendingMessage: {display: "none"}}>10:30</Text>
+              </TouchableOpacity>
             </View>
-            <Text style={(seeHourSend)? styles.sendingMessage: {display: "none"}}>10:30</Text>
-          </TouchableOpacity>
-        </View>
-        <View style={styles.containerMessageOwner}>
-          <TouchableOpacity onPress={() => onMessageClick()}>
-            <View style={styles.boxMessageOwner}>
-              <Text style={styles.messageOwner}>Bonjour Idris 👋🏼 {"\n"}Super. Belle journée également</Text>
+          ))}
+        </ScrollView>
+        <KeyboardAvoidingView behavior= 'position'>
+          <View style={styles.containerField}>
+            <View style={styles.leftContainerField}>
+              <Ionicons
+                name={'add-circle'}
+                size={30} color={'#7791DE'}
+                // style={styles.returnButton}
+                // onPress={() => props.navigation.navigate('HomePage')}
+              />
+              <TextInput
+                style={styles.textField}
+                placeholder="Text Message"
+                onChangeText={(value) => setMessageInput(value)}
+                value={messageInput}
+              />
             </View>
-            <Text style={(seeHourSend)? styles.sendingMessage: {display: "none"}}>10:30</Text>
-          </TouchableOpacity>
-        </View>
-      </ScrollView>
-      <KeyboardAvoidingView behavior= 'position'>
-        <View style={styles.containerField}>
-          <View style={styles.leftContainerField}>
             <Ionicons
-              name={'add-circle'}
+              name={'paper-plane-outline'}
               size={30} color={'#7791DE'}
-              // style={styles.returnButton}
+              style={styles.sendIcon}
               // onPress={() => props.navigation.navigate('HomePage')}
             />
-            <TextInput
-              style={styles.textField}
-              placeholder="Text Message"
-              onChangeText={(value) => setMessageInput(value)}
-              value={messageInput}
-            />
           </View>
-          <Ionicons
-            name={'paper-plane-outline'}
-            size={30} color={'#7791DE'}
-            style={styles.sendIcon}
-            // onPress={() => props.navigation.navigate('HomePage')}
-          />
-        </View>
-      </KeyboardAvoidingView>
-      <View style={{marginBottom: 30}}/>
-    </View>
-  )
+        </KeyboardAvoidingView>
+        <View style={{marginBottom: 30}}/>
+      </View>
+    )
+  }else {
+    return (
+      <View>
+        <Text>nul!</Text>
+      </View>
+    )
+  }
 }
+
+function mapStateToProps(state){
+  return{
+    conversationId: state.conversationId
+  }
+}
+  
+export default connect(mapStateToProps, null)
+(ChatPage);
 
 const styles = StyleSheet.create({
   container: {
@@ -143,7 +180,7 @@ const styles = StyleSheet.create({
   },
   containerField: {
     marginBottom: 10,
-    width: 350,
+    width: 375,
     height: 40,
     flexDirection: "row",
     borderWidth: 1,
@@ -157,7 +194,7 @@ const styles = StyleSheet.create({
     marginLeft: 10,
   },
   textField: {
-    width: 265,
+    width: 290,
     fontSize: 17,
     paddingLeft: 10,
   },
